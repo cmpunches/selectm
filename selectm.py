2a82dbd3280406a207b6db9c492bb7195268acaa
Chris Punches <bagira@forge.silogroup.org>
2020-12-07 14:44:55 -0500

    -

--- selectm.py
+++ selectm.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from configparser import ConfigParser
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class ConfLoader:
+    def __init__( self, conf_path ):
+        parser = ConfigParser()
+        parser.read( conf_path )
+        self.site_url = parser.get( 'targeting', 'site_url' )
+        self.brand_id = parser.get( 'targeting', 'brand_id' )
+        self.username = parser.get( 'user', 'username' )
+        self.password = parser.get( 'user', 'password' )
+
+
+def Main():
+    conf = ConfLoader('Conf/configuration.ini')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    Main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
