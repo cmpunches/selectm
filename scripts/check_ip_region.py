d251a6b1a1aafbf80a697056fde1d2d24470dd4d
Chris Punches <bagira@forge.silogroup.org>
2020-12-15 21:45:16 -0500

    ip_range checks

---
+++ scripts/check_ip_region.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from ipaddress import ip_network, ip_address
+import json
+
+
+def find_aws_region(ip):
+    ip_json = json.load(open('ip-ranges.json'))
+    prefixes = ip_json['prefixes']
+    my_ip = ip_address(ip)
+    region = 'Unknown'
+
+    for prefix in prefixes:
+        if my_ip in ip_network(prefix['ip_prefix']):
+            region = prefix['region']
+            print(region)
+            return region
+    return region
+
+
+find_aws_region( sys.argv[1] )
